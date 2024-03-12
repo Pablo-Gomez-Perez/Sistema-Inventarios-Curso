9e5d45c98330e674bf974f50514d8b02f20f98bc
pablog97 <pablojesus34@hotmail.com>
2024-03-12 17:42:21 -0600

    first-Commit

---
+++ SistemaInventarios/SistemaInventarios.Models/Bodega.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventarios.Models
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Nombre requerido")]
+        [MaxLength(60, ErrorMessage = "No mas de 60 caracteres")]
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Indique una descripción")]
+        [MaxLength(400, ErrorMessage = "Mucho texto we")]
+        [Display(Name = "Descripción")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Indique el estado")]
+        [Display(Name = "Estado")]
+        public bool State { get; set; }
+    }
+}
---
+++ SistemaInventarios/SistemaInventarios.Utils/Classes/DS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventarios.Utils.Classes
+{
+    internal static class DS
+    {
+    }
+}
